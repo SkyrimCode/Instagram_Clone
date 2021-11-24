cc2a592194d593d953a60c77f488e1943b0b62a8
Rajdeep Saha <rsaha734@gmail.com>
2021-11-24 22:31:59 +0530

    Added Stories Component

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,17 @@
-import Image from "next/image"
-import {SearchIcon,PlusCircleIcon,UserGroupIcon,HeartIcon,PaperAirplaneIcon,MenuIcon} from "@heroicons/react/outline"
-import { HomeIcon } from "@heroicons/react/solid"
+import Head from "next/head"
+import Feed from "../components/Feed"
+import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
-      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
-          {/* {Left} */}
-          <div className="relative hidden lg:inline-grid w-24">
-              <Image 
-                src="https://drive.google.com/uc?export=view&id=19agDVj5miAdZlO0FmvjPqZNS7WpsKM3o"
-                layout="fill"
-                objectFit="contain"
-              />
-          </div>
-          <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
-          <Image 
-                src="https://drive.google.com/uc?export=view&id=15d__d2s7ohXNLFnPagYjRIcCh4euTFRa"
-                layout="fill"
-                objectFit="contain"
-              />
-          </div>
-          {/* {Middle - Search Input Field} */}
-          <div className="max-w-xs">
-            <div className="relative mt-1 p-3 rounded-md">
-              <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                  <SearchIcon className="h-5 w-5 text-gray-500"/>
-              </div>
-                  <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
-                  type="text" placeholder="Search" />
-              </div>
-          </div>
-        
-          {/* {Right} */}
-          <div className="flex items-center justify-end space-x-4">
-          <HomeIcon className="navBtn"/>
-          <MenuIcon className="h-6 md:hidden cursor-pointer"/>
-          <div className="relative navBtn">
-            <PaperAirplaneIcon className="navBtn rotate-45"/>
-            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
-          </div>
-          <PlusCircleIcon className="navBtn"/>
-          <UserGroupIcon className="navBtn"/>
-          <HeartIcon className="navBtn"/>
-          <img src="https://drive.google.com/uc?export=view&id=1Sw0CzeXJh3LlyhBgyzOug5J8OYmFAc4A" className="h-10 rounded-full cursor-pointer"/>
-          </div>
-      
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
+      <Head>
+        <title>Instagram</title>
+      </Head>
 
-      {/* {Feed} */}
-
+      <Header/>
+       <Feed/>
       {/* {Modal} */}
-      </div>
     </div>
   )
 }
