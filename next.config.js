9ea9d3e70790c603a1f619918fa7e34bf4830c42
Rajdeep Saha <rsaha734@gmail.com>
2021-12-08 18:35:19 +0530

    Configured env variables 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     images: {
         domains: ["drive.google.com"]
-    }
-    // env: {
-    //     UID: process.env.UID,
-    //   }
+    },
+    env: {
+        UID: process.env.UID,
+        SECRET: 'T4tzN7XGIlFFboC5UFD/pdEntnHGbX+Rh/My3NjaQoU='
+      }
 }
