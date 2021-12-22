7b607471040ae040d5e44fe9f88a56355e4f5c23
Rajdeep Saha <rsaha734@gmail.com>
2021-12-22 22:43:04 +0530

    Added upload Post for resposive design

--- components/Header.js
+++ components/Header.js
@@ -47,8 +47,9 @@ function Header() {
           
             {/* {Right} */}
             <div className="flex items-center justify-end space-x-4">
-            <HomeIcon onClick={()=>router.push('/')} className="navBtn"/>
-            <MenuIcon className="h-6 md:hidden cursor-pointer"/>
+            <HomeIcon onClick={()=>router.push('/')} className="navBtn h-6 invisible md:visible cursor-pointer"/>
+            <PlusCircleIcon onClick={()=>setOpen(true)} className="h-12 md:hidden cursor-pointer"/>
+            
            
            {session? (
              <>
